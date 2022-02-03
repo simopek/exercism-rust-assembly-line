e4dbdb1a1a05da824568137087cafcec2b89d158
simopek <>
2022-02-03 23:08:49 +0100

    Add code

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+// This stub file contains items which aren't used yet; feel free to remove this module attribute
+// to enable stricter warnings.
+#![allow(unused)]
+
+pub fn production_rate_per_hour(speed: u8) -> f64 {
+    f64::from(speed) * 221.0 * match speed {
+        1..=4 => 1.0,
+        5..=8 => 0.9,
+        _ => 0.77
+    }
+}
+
+pub fn working_items_per_minute(speed: u8) -> u32 {
+    (production_rate_per_hour(speed) / 60.0) as u32
+}
